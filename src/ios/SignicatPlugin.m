ac271c4f69a467863c7c9a8eda7e27419b3d6dab
apalmaPL <apalma@product-league.com>
2025-11-17 15:27:17 +0100

    Update SignicatPlugin.m

--- src/ios/SignicatPlugin.m
+++ src/ios/SignicatPlugin.m
@@ -1,38 +1,80 @@
-#import "SignicatPlugin.h"
 #import "Cordova/CDV.h"
-
-@import SignicatSDK; 
+#import "SignicatPlugin.h"
+#import <ConnectisSDK/ConnectisSDK.h>  
 
 @implementation SignicatPlugin
 
-- (void)startAuthentication:(CDVInvokedUrlCommand*)command {
+- (void)login:(CDVInvokedUrlCommand*)command {
+    NSDictionary *config = [command.arguments objectAtIndex:0];
+    NSString *issuer = config[@"issuer"];
+    NSString *clientId = config[@"clientId"];
+    NSString *redirectUri = config[@"redirectUri"];
+    NSString *loginFlow = config[@"loginFlow"] ?: @"WEB";
+    BOOL allowDeviceAuth = [config[@"allowDeviceAuthentication"] boolValue];
+    NSString *brokerAppAcs = config[@"brokerAppAcs"];
+    NSString *brokerDigidAppAcs = config[@"brokerDigidAppAcs"];
 
-    NSDictionary* config = [command.arguments objectAtIndex:0];
-    NSString* clientId = config[@"clientId"];
-    NSString* redirectUri = config[@"redirectUri"];
-    NSString* env = config[@"environment"];
+    ConnectisSDKConfiguration *cfg = [[ConnectisSDKConfiguration alloc] initWithIssuer:issuer
+                                                                               clientID:clientId
+                                                                            redirectURI:redirectUri
+                                                                              loginFlow:( [loginFlow isEqualToString:@"APP_TO_APP"] ?
+                                                                                          ConnectisLoginFlowAppToApp :
+                                                                                          ConnectisLoginFlowWeb )];
+    cfg.allowDeviceAuthentication = allowDeviceAuth;
+    if (brokerAppAcs) {
+        cfg.brokerAppAcs = brokerAppAcs;
+    }
+    if (brokerDigidAppAcs) {
+        cfg.brokerDigidAppAcs = brokerDigidAppAcs;
+    }
 
-    SignicatConfig* sc = [[SignicatConfig alloc] initWithClientId:clientId
-                                                      redirectUri:redirectUri
-                                                      environment:[env isEqual:@"TEST"] ?
-                                                            SignicatEnvironmentTest :
-                                                            SignicatEnvironmentProduction];
+    [ConnectisSDK initializeWithConfiguration:cfg];
 
-    [[SignicatSDK shared] initialize:sc];
+    __weak SignicatPlugin *weakSelf = self;
+    [ConnectisSDK loginFromViewController:self.viewController
+                                  success:^(ConnectisAuthenticationResponse *response) {
 
-    __weak SignicatPlugin* weakSelf = self;
+        NSMutableDictionary *res = [NSMutableDictionary dictionary];
+        res[@"issuer"] = response.issuer;
+        res[@"accessToken"] = response.accessToken;
+        res[@"idToken"] = response.idToken;
+        res[@"refreshToken"] = response.refreshToken;
 
-    [[SignicatSDK shared] startAuthenticationFrom:self.viewController
-        success:^(NSString *token) {
-            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                                                        messageAsString:token];
-            [weakSelf.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                                       messageAsDictionary:res];
+        [weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+    } error:^(NSError *error) {
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                       messageAsString:error.localizedDescription];
+        [weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+- (void)getAccessToken:(CDVInvokedUrlCommand*)command {
+    __weak SignicatPlugin *weakSelf = self;
+    [ConnectisSDK getOpenIdAccessToken:^(NSString *token, NSError *error) {
+        if (token) {
+            CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:token];
+            [weakSelf.commandDelegate sendPluginResult:res callbackId:command.callbackId];
+        } else {
+            CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+            [weakSelf.commandDelegate sendPluginResult:res callbackId:command.callbackId];
         }
-        error:^(NSError *err) {
-            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                                                        messageAsString:err.localizedDescription];
-            [weakSelf.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-        }];
+    }];
+}
+
+- (void)enableDeviceAuth:(CDVInvokedUrlCommand*)command {
+    [ConnectisSDK enableDeviceAuthentication];
+    CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"deviceAuthEnabled"];
+    [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
+}
+
+- (void)disableDeviceAuth:(CDVInvokedUrlCommand*)command {
+    [ConnectisSDK disableDeviceAuthentication];
+    CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"deviceAuthDisabled"];
+    [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
 }
 
 @end
+
