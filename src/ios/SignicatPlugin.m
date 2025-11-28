60d243c9979930fcabbcb374e128f8a7321a755b
apalmaPL <apalma@product-league.com>
2025-11-28 18:58:10 +0100

    Update SignicatPlugin.m

--- src/ios/SignicatPlugin.m
+++ src/ios/SignicatPlugin.m
@@ -16,6 +16,12 @@
     NSString *brokerAppAcs = config[@"brokerAppAcs"];
     NSString *brokerDigidAppAcs = config[@"brokerDigidAppAcs"];
 
+    ConnectisSDKConfiguration *cfg = [[ConnectisSDKConfiguration alloc] initWithIssuer:issuer
+                                                                               clientID:clientId
+                                                                            redirectURI:redirectUri
+                                                                              loginFlow:( [loginFlow isEqualToString:@"APP_TO_APP"] ?
+                                                                                          ConnectisLoginFlowAppToApp :
+                                                                                          ConnectisLoginFlowWeb )];
 
 }
 
