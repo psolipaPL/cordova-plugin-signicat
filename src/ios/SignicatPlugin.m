c4ef996b3312be9a6a7cc953fc28b117e5cf5d42
apalmaPL <apalma@product-league.com>
2025-11-28 18:53:43 +0100

    Update SignicatPlugin.m

--- src/ios/SignicatPlugin.m
+++ src/ios/SignicatPlugin.m
@@ -6,5 +6,20 @@
 
 @implementation SignicatPlugin
 
+- (void)login:(CDVInvokedUrlCommand*)command {
+    NSDictionary *config = [command.arguments objectAtIndex:0];
+    NSString *issuer = config[@"issuer"];
+    NSString *clientId = config[@"clientId"];
+    NSString *redirectUri = config[@"redirectUri"];
+    NSString *loginFlow = config[@"loginFlow"] ?: @"WEB";
+    BOOL allowDeviceAuth = [config[@"allowDeviceAuthentication"] boolValue];
+    NSString *brokerAppAcs = config[@"brokerAppAcs"];
+    NSString *brokerDigidAppAcs = config[@"brokerDigidAppAcs"];
+
+
+}
+
+
+
 @end
 
