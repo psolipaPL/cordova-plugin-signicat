ad737d0362f4e63b98bf85f74985c8d518e49f97
apalmaPL <apalma@product-league.com>
2025-12-02 15:15:55 +0100

    Add files via upload

---
+++ src/ios/ConnectisSDK.framework/Headers/ConnectisSDK.h
@@ -0,0 +1,18 @@
+//
+//  ConnectisSDK.h
+//  ConnectisSDK
+//
+//  Created by Dan Cristian Cananau on 21.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConnectisSDK.
+FOUNDATION_EXPORT double ConnectisSDKVersionNumber;
+
+//! Project version string for ConnectisSDK.
+FOUNDATION_EXPORT const unsigned char ConnectisSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConnectisSDK/PublicHeader.h>
+
+
