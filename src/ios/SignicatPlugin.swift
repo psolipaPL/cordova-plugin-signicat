b51d3677a3745d39b1c8d8f5be1d6883cf08d4f9
apalmaPL <apalma@product-league.com>
2025-12-03 12:28:39 +0100

    Update SignicatPlugin.swift

--- src/ios/SignicatPlugin.swift
+++ src/ios/SignicatPlugin.swift
@@ -4,39 +4,10 @@ import ConnectisSDK
 @objc(SignicatPlugin)
 class SignicatPlugin: CDVPlugin {
 
-    private var callbackId: String?
 
     @objc(login:)
     func login(command: CDVInvokedUrlCommand) {
-        self.callbackId = command.callbackId
-
-        guard let config = command.arguments.first as? [String: Any] else {
-            sendError("Invalid config", command.callbackId)
-            return
-        }
-        // Extract parameters
-        let issuer = config["issuer"] as? String ?? ""
-        let clientId = config["clientId"] as? String ?? ""
-        let redirectUri = config["redirectUri"] as? String ?? ""
-        let scopes = config["scopes"] as? String
-        let brokerAppAcs = config["brokerAppAcs"] as? String
-        let brokerDigidAppAcs = config["brokerDigidAppAcs"] as? String
-        let loginFlowStr = config["loginFlow"] as? String ?? "WEB"
-        let loginFlow: LoginFlow = (loginFlowStr == "APP_TO_APP") ? .appToApp : .web
-        let allowDeviceAuth = config["allowDeviceAuthentication"] as? Bool ?? false
-
-        // Build SDK configuration
-        var sdkConfig = ConnectisSDKConfiguration(
-            issuer: issuer,
-            clientID: clientId,
-            redirectURI: redirectUri,
-            scopes: scopes,
-            brokerAppAcs: brokerAppAcs,
-            brokerDigidAppAcs: brokerDigidAppAcs,
-            loginFlow: loginFlow
-        )
-
-
+        
     }
 
 
