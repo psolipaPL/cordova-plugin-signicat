1326d2da2180774a35a044b2a8b9af017a314957
apalmaPL <apalma@product-league.com>
2025-12-03 12:59:53 +0100

    Update SignicatPlugin.swift

--- src/ios/SignicatPlugin.swift
+++ src/ios/SignicatPlugin.swift
@@ -4,10 +4,18 @@ import ConnectisSDK
 
 class SignicatPlugin: CDVPlugin {
 
-    func wtf() {
-        var isDeviceAuthEnabled = ConnectisSDK.isDeviceAuthenticationEnabled()
-
-        return isDeviceAuthEnabled
+    
+    func loginAppToApp() {
+        let configuration = ConnectisSDKConfiguration.init(
+            issuer: connectisConfiguration.issuer,
+            clientID: connectisConfiguration.clientID,
+            redirectURI: connectisConfiguration.appToAppRedirectURI,
+            scopes: connectisConfiguration.appToAppScopes,
+            brokerDigidAppAcs: connectisConfiguration.brokerDigidAppAcs,
+            loginFlow: LoginFlow.APP_TO_APP)
+        
+        ConnectisSDK.logIn(sdkConfiguration: configuration, caller: self, delegate: responseController, allowDeviceAuthentication: ConnectisSDK.isDeviceAuthenticationEnabled())
     }
+    
 }
 
