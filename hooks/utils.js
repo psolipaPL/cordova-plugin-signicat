c8f3e56f26e27f72439024827ef62627c18fbecf
Adriano Palma <neozarp@gmail.com>
2025-12-29 12:17:30 +0000

    sdad

--- hooks/utils.js
+++ hooks/utils.js
@@ -30,7 +30,7 @@ function readFile(filePath) {
 
 function indexReplacer(indexPath, content) {
 
-    content = content.replace('<script type="text/javascript" src="scripts/cordova.js', '<script>function handleOpenURL(url) {console.log(url);}</script><script type="text/javascript" src="scripts/cordova.js');
+    content = content.replace('<script type="text/javascript" src="scripts/cordova.js', '<script type="text/javascript">function handleOpenURL(url) {setTimeout(function(){console.log(url);},0);}</script><script type="text/javascript" src="scripts/cordova.js');
     console.log('Added openURLhandler JS function.')
 
     fs.writeFileSync(indexPath, content, "utf-8");
